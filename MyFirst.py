8b8f76d95f3308ae4abe9338e8a8854d2c8ee1fb
Rafezlyn97 <101541294+Rafezlyn97@users.noreply.github.com>
2022-07-21 12:26:36 +0800

    Update MyFirst.py

--- MyFirst.py
+++ MyFirst.py
@@ -3,8 +3,9 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.header("My first Streamlit App")
 st.title("This is a title")
+st.header("My first Streamlit App")
+
 
 st.write("""
 # This is a first-level heading
@@ -15,3 +16,9 @@ st.write(pd.DataFrame({
     'Intplan': ['yes', 'yes', 'yes', 'no'],
     'Churn Status': [0, 0, 0, 1]
 }))
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
