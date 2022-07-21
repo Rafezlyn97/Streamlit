a24144e698e8ec25ccd59ad6f3137e17bf45a3a1
Rafezlyn97 <101541294+Rafezlyn97@users.noreply.github.com>
2022-07-21 12:28:09 +0800

    Update MyFirst.py

--- MyFirst.py
+++ MyFirst.py
@@ -5,8 +5,6 @@ import pandas as pd
 
 st.title("This is a title")
 st.header("My first Streamlit App")
-
-
 st.write("""
 # This is a first-level heading
 ## This is a second-level heading
@@ -17,8 +15,16 @@ st.write(pd.DataFrame({
     'Churn Status': [0, 0, 0, 1]
 }))
 
+st.header("Line Chart")
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+st.header("Map")
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
